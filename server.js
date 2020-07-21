74a3b956d1feacce60f159ab3f788933e202c7c8
Vijay <66685378+vijaygupta18@users.noreply.github.com>
2020-07-21 23:21:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var PORT = process.env.PORT || 3000; // take port from heroku or for loacalhost
+var PORT = process.env.PORT || 3000; 
 var express = require("express");
 var app = express(); // express app which is used boilerplate for HTTP
 var http = require("http").Server(app);
 
-//moment js
+
 var moment = require("moment");
 
 var clientInfo = {};
@@ -11,7 +11,7 @@ var clientInfo = {};
 //socket io module
 var io = require("socket.io")(http);
 
-// expose the folder via express thought
+
 app.use(express.static(__dirname + '/public'));
 
 // send current users to provided scoket
@@ -31,7 +31,7 @@ function sendCurrentUsers(socket) { // loading current users
     }
 
   });
-  // emit message when all users list
+ 
 
   socket.emit("message", {
     name: "System",
@@ -50,8 +50,7 @@ io.on("connection", function(socket) {
   socket.on("disconnect", function() {
     var userdata = clientInfo[socket.id];
     if (typeof(userdata !== undefined)) {
-      socket.leave(userdata.room); // leave the room
-      //broadcast leave room to only memebers of same room
+      socket.leave(userdata.room); 
       socket.broadcast.to(userdata.room).emit("message", {
         text: userdata.name + " has left",
         name: "System",
@@ -79,7 +78,7 @@ io.on("connection", function(socket) {
 
   // to show who is typing Message
 
-  socket.on('typing', function(message) { // broadcast this message to all users in that room
+  socket.on('typing', function(message) { 
     socket.broadcast.to(clientInfo[socket.id].room).emit("typing", message);
   });
 
@@ -91,12 +90,12 @@ io.on("connection", function(socket) {
   });
 
   socket.emit("message", {
-    text: "Welcome to Chat Appliction !",
+    text: "Welcome to Gossip-Dot-Com !",
     timestamp: moment().valueOf(),
     name: "System"
   });
 
-  // listen for client message
+  
   socket.on("message", function(message) {
     console.log("Message Received : " + message.text);
     // to show all current users
@@ -105,10 +104,10 @@ io.on("connection", function(socket) {
     } else {
       //broadcast to all users except for sender
       message.timestamp = moment().valueOf();
-      //socket.broadcast.emit("message",message);
+      
       // now message should be only sent to users who are in same room
       socket.broadcast.to(clientInfo[socket.id].room).emit("message", message);
-      //socket.emit.to(clientInfo[socket.id].room).emit("message", message);
+      
     }
 
   });
